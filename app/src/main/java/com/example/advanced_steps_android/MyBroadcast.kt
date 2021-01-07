d4f7ad37f61422faaee8ac0f91c791c8e5963daf
Beatriz Arruda de Castro <b.arruda.de.castro@avanade.com>
2021-01-07 13:42:36 -0300

    notification exercise

--- app/src/main/java/com/example/advanced_steps_android/MyBroadcast.kt
+++ app/src/main/java/com/example/advanced_steps_android/MyBroadcast.kt
@@ -1,15 +1,38 @@
 package com.example.advanced_steps_android
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 
 class MyBroadcast: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val data = intent?.getStringExtra("DATA")
-       Toast.makeText(context, "Eu sou o MyBroadcast executando ... $data", Toast.LENGTH_LONG).show()
+        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val mChannel = NotificationChannel("123", "my_channel", NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(mChannel)
+        }
+
+        val intent2 = Intent(context, ActivityFilter::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        val pendingIntent = PendingIntent.getActivity(context, 0,  intent2, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(context, "123")
+            .setSmallIcon(android.R.drawable.ic_input_add)
+            .setContentTitle("Alarme executado")
+            .setContentText("Conteudo do alarme agendado")
+            .setContentIntent(pendingIntent)
+
+        notificationManager.notify(123 , builder.build())
     }
 }
